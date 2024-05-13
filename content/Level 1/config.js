637338cdc07feed991e29e2c6a27f6c8fa30cf08
Manjara Raj <153396359+Mnj-ToTheTop@users.noreply.github.com>
2024-05-13 13:38:13 +0530

    Update config.js

---
+++ content/Level 1/config.js
@@ -0,0 +1,10 @@
+// So that if we modify the code, we won't need to modify everywhere
+
+export const level1Config = {
+    gravity: 1400,
+    playerSpeed: 400,
+    jumpForce: 650,
+    nbLives: 3,
+    playerStartPosX: 1500,
+    playerStartPosY: 100,
+}
