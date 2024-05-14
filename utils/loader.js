9a46f8a8d10d54e61967d2e1e891d73bd078462e
Manjara Raj <153396359+Mnj-ToTheTop@users.noreply.github.com>
2024-05-14 19:04:37 +0530

    Update loader.js

--- utils/loader.js
+++ utils/loader.js
@@ -49,22 +49,6 @@ export const load = {
             }
         })
 
-        //loadSprite("lava", "./assets/Lava.png", {
-          //  sliceX: 8,
-            //sliceY: 1,
-            //anims: {
-              //  wave: {
-                //    from: 0, to: 7,
-                  //  speed: 16, loop: true,
-                //},
-
-                //"wave-reversed": {
-                  //  from: 7, to: 0,
-                    //speed: 16, loop: true,
-                //},
-           // }
-        //})
-
         loadSprite("grass-tileset", "./assets/Grass_Tileset.png", {
             sliceX: 3,
             sliceY: 4,
