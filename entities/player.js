dcd2bc4a84444273de5fbb62b8ef8c6ff7b1274e
Manjara Raj <153396359+Mnj-ToTheTop@users.noreply.github.com>
2024-05-14 18:37:06 +0530

    Add files via upload

--- entities/player.js
+++ entities/player.js
@@ -1,16 +1,18 @@
+
 export class Player
 {
-    
     heightDelta = 0
     isRespawning = false
     isMoving = false
-    
+    coyoteLapse = 0.1
+    coins = 0
+
     constructor(posX, 
         posY, speed, jumpForce, 
         nbLives, currentLevelScene, 
-        isInTerminalScene) //Can use vec2 as position intead of individual positions
+        inInFinalLevel) //Can use vec2 as position intead of individual positions
     {
-        this.isInTerminalScene = isInTerminalScene
+        this.inInFinalLevel = inInFinalLevel
         this.currentLevelScene = currentLevelScene
         this.initialX = posX    //Useful for respawning
         this.initialY = posY    //Useful for respawning
@@ -25,10 +27,10 @@ export class Player
     makePlayer(x, y)
     {
         this.gameObj = add([
-            sprite("player", {anime: "idle"}),
+            sprite("player", {anim: "idle"}),
             area({ shape: new Rect(vec2(0,3), 8,8)}),
             anchor("center"),
-            pos(x, y),
+            pos(x,y),
             scale(4),
             body(),
             "player",
@@ -51,6 +53,15 @@ export class Player
 
     }
 
+    enableCoinPickUp()
+    {
+        this.gameObj.onCollide("coin", (coin_collected) => {
+            this.coins++
+            destroy(coin_collected)
+            play("coin")
+        })
+    }
+
     setPlayerControls()
     {
         onKeyDown("left", () => {
@@ -70,11 +81,15 @@ export class Player
         })
 
         onKeyDown("space", () => {
-            if (this.gameObj.isGrounded())
-            {
-                if(!this.isRespawning) this.gameObj.jump(this.jumpForce)
-                play("jump-sound")
-            }
+
+            if(!this.gameObj.isGrounded() && this.hasJumpedOnce) return
+
+            if(time() - this.timeSinceLastGrounded > this.coyoteLapse) return
+
+            this.gameObj.jump(this.jumpForce)
+            play("jump-sound")
+            this.hasJumpedOnce = true
+
         })
 
         onKeyRelease(() => {
@@ -87,20 +102,29 @@ export class Player
     }
 
     respawnPlayer() 
-    {
-        if (this.lives > 0)
         {
-            this.lives = this.lives - 1
-            this.gameObj.pos = vec2(this.initialX, this.initialY)
-            this.isRespawning = true
-            setTimeout(()=> this.isRespawning = false, 500)
+            if (this.lives > 1)
+            {
+                this.lives--
+                this.gameObj.pos = vec2(this.initialX, this.initialY)
+                this.isRespawning = true
+                setTimeout(()=> this.isRespawning = false, 500)
+                return
+            }
+
+            go("gameOver")
         }
-    }
         
 
     update()
     {
         onUpdate(() => {
+
+            if(this.gameObj.isGrounded())
+            {
+                this.hasJumpedOnce = false
+                this.timeSinceLastGrounded = time()
+            }
             this.heightDelta = this.previousHeight - this.gameObj.pos.y
             this.previousHeight = this.gameObj.pos.y
             
@@ -112,20 +136,20 @@ export class Player
             
             if(this.gameObj.pos.y > 800)
             {
-                // Sound: play("hit", { speed: 1.5})
+                play("hit", { speed: 1.5})
                 this.respawnPlayer()
             }
             
             if(!this.gameObj.isGrounded() &&
                 this.heightDelta > 0 &&
-                this.gameObj.curAnim() != "jump-up")   // Jumping up
+                this.gameObj.curAnim() !== "jump-up")   // Jumping up
             {
                 this.gameObj.play("jump-up")
             }
 
             if(!this.gameObj.isGrounded() &&
                 this.heightDelta < 0 &&
-                this.gameObj.curAnim() != "jump-down")   // Jumping up
+                this.gameObj.curAnim() !== "jump-down")   // Jumping up
             {
                 this.gameObj.play("jump-down")
             }
@@ -133,4 +157,22 @@ export class Player
             
         })
     }
+
+    updateLivesCount(livesCountUI)
+    {
+        onUpdate( () => {
+            livesCountUI.text = this.lives
+        })
+    }
+    updateCoinCount(coinCountUI)
+    {
+        onUpdate( () => {
+            coinCountUI.text = `${this.coins} / ${coinCountUI.fullCoinCount}`
+            if(this.coins === coinCountUI.fullCoinCount)
+            {
+                go(this.inInFinalLevel ? "endScreen" : this.currentLevelScene + 1)
+            }
+        })
+    }
+    
 }
